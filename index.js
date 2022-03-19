e7fad53ec8874a8f7e1ef6a8eee0688ac6b5f459
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-03-20 00:00:58 +0100

    All the output is clear

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ let outputFileName = prompt('Name the file where you want to output your data: '
 
 let fileExtention = /$\.json/;
 
-if(fileExtention.test(outputFileName) == true){
+if(fileExtention.test(outputFileName) === true){
     console.log(`Your data will be output throught the file ${outputFileName}`);
 } else {
     outputFileName = `${outputFileName}.json`;
@@ -25,13 +25,18 @@ if(fileExtention.test(outputFileName) == true){
 const projectIds = prompt("Enter the ids of the ressources you want to get separated by a space ' ':  ");
 console.log(`Your ids are: ${projectIds}`);
 
-projectIdsArr = projectIds.split('');
 
-let trimmedIds = []
-for(projectId in projectIds){
-    trimmedID = projectId.trim();
-    trimmedIds.push(trimmedID);
+let projectIdsArr = projectIds.trim().split(" ");
+//console.log(projectIdsArr);
+
+//Node does not support higher order functions so I did it the Java way
+let trimmedIds = [];
+for (let i = 0; i < projectIdsArr.length ; i++){
+    //console.log(projectIdsArr.length);
+    trimmedIds.push(projectIdsArr[i].trim());
 }
+
+console.log(trimmedIds);
   
 // // We get our data in chucnks in order to avoid buffering problems that happen usaly in the CLI
 //   https.get('', (resp) => {
