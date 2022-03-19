7126cbed55efba221f842c8ffb1da22ff2418a74
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-03-19 22:11:12 +0100

    added a filtering pattern to the second input of ids.

--- index.js
+++ index.js
@@ -1,24 +1,60 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const readline = require('readline');
+const prompt = require('prompt-sync')();
 //const token = require('Token.js')
 const app = express();
+
+
 const api_url = "";
 //const response = await fetch(api_url);
 let outputFile = "";
 
-const input = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+const outputFileName = prompt('Name the file where you want to output your data: ');
+
+let fileExtention = /$\.json/;
+
+if(outputFileName.test(fileExtention) == true){
+    console.log(`Your data will be output throught the file ${outputFileName}`);
+} else {
+    outputFileName = `${outputFileName}.json`;
+    console.log(`Your data will be output throught the file ${outputFileName}.json`);
+}
+
+
+const projectIds = prompt("Enter the ids of the ressources you want to get separated by a space ' ':  ");
+console.log(`Your ids are: ${projectIds}`);
+
+projectIdsArr = projectIds.split('');
+
+let trimmedIds = []
+for(projectId in projectIds){
+    trimmedID = projectId.trim();
+    trimmedIds.push(trimmedID);
+}
   
-  readline.question('Output File Name?', oFileName => {
-    console.log(`Output file name is: ${oFileName}!`);
-    readline.close();
-  });
+// // We get our data in chucnks in order to avoid buffering problems that happen usaly in the CLI
+//   https.get('', (resp) => {
+
+//   let data = ''; // we initialize a 'data' variable
+
+//   // A chunk of data has been received, and we add it to the previous one
+//   resp.on('data', (chunk) => {
+//     data += chunk;
+//   });
+
+//   // The whole response has been received. Print out the result.
+//   resp.on('end', () => {
+//     console.log(JSON.parse(data).explanation);
+//   });
+
+// }).on("error", (err) => {
+//   console.log("Error: " + err.message);
+// });
+// //End of getting data from the buffer
 
 
+// Making the directory
 fs.mkdir(path.join(__dirname, '/output'), {recursive : true}, err => {
     if(err) {
         throw err;
@@ -28,6 +64,7 @@ fs.mkdir(path.join(__dirname, '/output'), {recursive : true}, err => {
     }
 });
 
+ // Writing to file
 fs.writeFile(path.join(__dirname, '/output', 'o.json'), '${data}', err => {
     if(err) {
         throw err;
